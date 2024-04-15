0817fbc424c4ee8e22492b248f61454f7d8e953f
onlinearivu <garumugamtvm@gmail.com>
2024-04-16 01:43:21 +0530

    test Component

--- frontend/src/components/researchComponent.tsx
+++ frontend/src/components/researchComponent.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { clientService } from "../service/client";
+import { AppContext } from "../App";
 
-const ResearchComponent=()=>{
-    const getUsers=async()=>{
-        const response=await clientService('GET',"http://localhost:4000/api/users",{})
-        if(response?.status==200)
-            console.log('Response Data',response?.data)
+const ResearchComponent = () => {
+    const context = useContext(AppContext);
+    const getUsers = async () => {
+        const response = await clientService('GET', `${context.value}/api/users`, {})
+        if (response?.status == 200)
+            console.log('Response Data', response?.data)
     }
     return <div><button onClick={getUsers}>Get Users from API</button></div>
 }
